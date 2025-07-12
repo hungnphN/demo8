fe896c3e712c6b0ce3c5af17a19d2de6ca87f2f8
Hungnguyen <Hungphu2k2@gmail.com>
2025-07-12 21:25:52 +0700

    demo82

--- Assets/Source code/Combat/EnemySpawner.cs
+++ Assets/Source code/Combat/EnemySpawner.cs
@@ -6,10 +6,29 @@ public class EnemySpawner : MonoBehaviour
 {
     public EnemyWave[] enemyWaves;
     private int currentWave;
+    public Transform bossPrefab;
+    public FlyPath bossFlyPath;
+    public float bossSpeed = 2f;
+    public float delayBeforeBoss = 2f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave();
+        //SpawnEnemyWave();
+        StartCoroutine(spawnWavesthenBoss());
+    }
+    IEnumerator spawnWavesthenBoss()
+    {
+        while (currentWave < enemyWaves.Length)
+        {
+            SpawnEnemyWave();
+            float delay = enemyWaves[currentWave].nextWaveDelay;
+            currentWave++;
+            yield return new WaitForSeconds(delay);
+        }
+        yield return new WaitForSeconds(delayBeforeBoss);
+        SpawnBoss();
     }
     private void SpawnEnemyWave()
     {
@@ -23,12 +42,28 @@ public class EnemySpawner : MonoBehaviour
             agent.flySpeed = waveInfo.speed;
             startPosition += waveInfo.formationOffset;
             agent.isReady = true;
+            startPosition += waveInfo.formationOffset;
         }
-        currentWave++;
-        if(currentWave < enemyWaves.Length)
+        //currentWave++;
+        //if(currentWave < enemyWaves.Length)
+        //{
+        //    Invoke(nameof(SpawnEnemyWave), waveInfo.nextWaveDelay);
+        //}
+    }
+    private void SpawnBoss()
+    {
+        if (bossPrefab == null || bossFlyPath == null)
         {
-            Invoke(nameof(SpawnEnemyWave), waveInfo.nextWaveDelay);
+            Debug.LogWarning("Boss Prefab or flypath chua doc gan!");
+            return;
         }
+        Vector3 startPos = bossFlyPath.waypoints[0].transform.position;
+        var boss = Instantiate(bossPrefab, startPos, Quaternion.identity);
+        var agent = boss.GetComponent<FlyPathArgent>();
+        agent.flyPath = bossFlyPath;
+        agent.flySpeed = bossSpeed;
+        agent.isReady = true;
+        Debug.Log("Boss Spawned");
     }
     // Update is called once per frame
     void Update()
